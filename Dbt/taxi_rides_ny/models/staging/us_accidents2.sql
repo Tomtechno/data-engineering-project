94c04b32bc60e4a48cac8b6b86aec86b32c25622
Lucas Ro <uzquiano.tomas.usa@gmail.com>
2023-04-27 14:45:45 +0000

    Tables

--- Dbt/taxi_rides_ny/models/staging/us_accidents2.sql
+++ Dbt/taxi_rides_ny/models/staging/us_accidents2.sql
@@ -1,17 +1,4 @@
-{{ config(
-        materialized = "view",  
-        cluster_by = "state"
-    )
-}}
+{{ config(materialized = "view") }}
 
-select
-    cast(state as string) as state,
-    cast(ID as string) as accident_id,
-    cast(severity as integer) as severity_id,
-    cast(start_time as date) as start_time,
-    cast(end_time as date) as end_time,
-    cast(description as string) as description,
-    cast(weather_condition as string) as weather_condition
-    avg(severity_id as float) as severity_average
-from {{ source("staging", "us_accidents") }}
-group by state
+select * from {{ source("staging", "us_accidents") }}
+limit 50
